cb4443154018f8fe4b186b041b2c7ed0f1b19191
Johan Esteban Arias Arboleda <133719384+ArsJohan@users.noreply.github.com>
2025-03-12 19:54:40 -0500

    Delete VentasViviendas directory

--- VentasViviendas/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace VentasViviendas.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- VentasViviendas/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace VentasViviendas.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
